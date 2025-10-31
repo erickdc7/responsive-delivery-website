2e993c136bfe9ff69c91b8b5fe75e37ed59b1329
Erick Díaz <82739201+erickdc7@users.noreply.github.com>
2025-10-31 12:27:42 -0500

    add remove class list

--- assets/js/main.js
+++ assets/js/main.js
@@ -16,6 +16,7 @@ const navLink = document.querySelectorAll('.nav__link')
 const linkAction = () =>{
     const navMenu = document.getElementById('nav-menu')
     // When we click on each nav__link, we remove the show-menu class
+    navMenu.classList.remove('show-menu')
 }
 
 /*=============== ADD SHADOW HEADER ===============*/
