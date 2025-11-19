3825b282a3ff36340cd0e47dc3465b9b883d4e54
Erick Díaz <82739201+erickdc7@users.noreply.github.com>
2025-11-19 12:21:36 -0500

    add distance property

--- assets/js/main.js
+++ assets/js/main.js
@@ -113,4 +113,5 @@ themeButton.addEventListener('click', () => {
 /*=============== SCROLL REVEAL ANIMATION ===============*/
 const sr = ScrollReveal({
     origin: 'top',
+    distance: '60px',
 })
